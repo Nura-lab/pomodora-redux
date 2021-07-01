24779e9651bf47a763388531b6ab3a081fafc8a3
Nura-lab <erkinovanuraiym9@gmail.com>
2021-07-01 16:48:49 +0300

    Updates

---
+++ precache-manifest.792c5c2ca890724cd925cbd3d5bb6642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb0d0c9a6b83e5d718259e3b1a88ee2",
+    "url": "/pomodora-redux/index.html"
+  },
+  {
+    "revision": "1ab9dde01ea586cf55e7",
+    "url": "/pomodora-redux/static/css/main.03a27791.chunk.css"
+  },
+  {
+    "revision": "a478f393437665e88c70",
+    "url": "/pomodora-redux/static/js/2.11aa467a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/pomodora-redux/static/js/2.11aa467a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab9dde01ea586cf55e7",
+    "url": "/pomodora-redux/static/js/main.f430810a.chunk.js"
+  },
+  {
+    "revision": "3cef469b218afc5cbbfa",
+    "url": "/pomodora-redux/static/js/runtime-main.605fb71d.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/pomodora-redux/static/media/bell.525320df.flac"
+  }
+]);
